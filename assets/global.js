130b0506853bf4d1ab809326dfe846fe22989fa6
Adrian Kamiński <apkaminski@gmail.com>
2023-08-25 15:44:56 +0200

    fix
    Signed-off-by: Adrian Kamiński <apkaminski@gmail.com>

--- assets/global.js
+++ assets/global.js
@@ -9,10 +9,7 @@ function getFocusableElements(container) {
 function launchKlavyoPopup(event, _desktopID, _mobileID) {
   event.preventDefault();
   window._klOnsite = window._klOnsite || [];
-  if (
-    _mobileID &&
-    (navigator.userAgentData.mobile || window.innerWidth <= 768)
-  ) {
+  if (_mobileID && window.innerWidth <= 768) {
     console.log('mobile');
     window._klOnsite.push(['openForm', _mobileID]);
   } else {
